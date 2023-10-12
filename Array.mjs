35b58722f1031216462b2a9dfb11268736529d82
beesan-ahmad <abumfarreh@gmail.com>
2023-10-12 11:49:25 +0300

    fixed a bug caused by search by name method

--- Array.mjs
+++ Array.mjs
@@ -57,31 +57,39 @@ class Array {
     }
 
     // In this method we search for a country and return all results that close to the input 
-    searchByCountry(input) {
-        input = input.toLowerCase();
-        let result = this.#data.filter(country => country.toLowerCase().includes(input));
-        return result;
-    }
- /*
- This function looks for a country and returns exactly the same result. I also check out for
-  attempts by the user to enter erroneous data, such as null or not a string, and I notify him 
-  if the input he entered to search for is not found. 
- */
-    searchExact(input1) {
+    searchByName(input1) {
         if (!input1 || typeof input1 !== 'string') {
-          return "Please enter a valid search input.";
+            return "Please enter a valid search input.";
         }
-    // Convert the input1 to lowercase for case-insensitive matching 
-        input1 = input1.toLowerCase(); 
-        let resultsOfExactSearch = this.#data.filter(country => country.toLowerCase() === input1);
-    
+        input1 = input1.toLowerCase();
+        let resultOfSearch = this.#data.filter(country => country.toLowerCase().includes(input1));
+        if (resultOfSearch.length === 0) {
+            return `No results found for "${input1}".`;
+        }
+        return resultOfSearch;
+
+
+    }
+    /*
+    This function looks for a country and returns exactly the same result. I also check out for
+     attempts by the user to enter erroneous data, such as null or not a string, and I notify him 
+     if the input he entered to search for is not found. 
+    */
+    searchExact(input2) {
+        if (!input2 || typeof input2 !== 'string') {
+            return "Please enter a valid search input.";
+        }
+        // Convert the input1 to lowercase for case-insensitive matching 
+        input2 = input2.toLowerCase();
+        let resultsOfExactSearch = this.#data.filter(country => country.toLowerCase() === input2);
+
         if (resultsOfExactSearch.length === 0) {
-          return `No results found for "${input1}".`;
+            return `No results found for "${input2}".`;
         }
-    
+
         return resultsOfExactSearch;
-      }
-    
+    }
+
 }
 
 let countryData = ["palestine", "jordan", "syria", "lebanon"];
@@ -96,12 +104,12 @@ console.log(countries.getSortedData());
 countries.decreasingSort(); // Sort the array in decreasing order
 console.log("Sorted array in decreasing order:");
 console.log(countries.getSortedData());
-let searchItem = "i"; // the output will be syria and jordan
-let searchResults = countries.searchByCountry(searchItem);
-console.log(`Search results for "${searchItem}":`);
+let searchItem1 = "z"; // no result found
+let searchResults = countries.searchByName(searchItem1);
+console.log(`Search results for "${searchItem1}":`);
 console.log(searchResults);
 
-let searchItem2 = "iraq"; 
+let searchItem2 = "iraq"; // no result found
 let exactSearchResults = countries.searchExact(searchItem2);
 console.log(`Exact Search results : "${searchItem2}":`);
 console.log(exactSearchResults);
