a28a225542645a3be9313d4bc7bdd1c670290652
beesan-ahmad <abumfarreh@gmail.com>
2023-10-12 11:07:10 +0300

    search for a country and return the result that perfectly matched with the input

--- Array.mjs
+++ Array.mjs
@@ -57,12 +57,31 @@ class Array {
     }
 
     // In this method we search for a country and return all results that close to the input 
-
     searchByCountry(input) {
         input = input.toLowerCase();
         let result = this.#data.filter(country => country.toLowerCase().includes(input));
         return result;
     }
+ /*
+ This function looks for a country and returns exactly the same result. I also check out for
+  attempts by the user to enter erroneous data, such as null or not a string, and I notify him 
+  if the input he entered to search for is not found. 
+ */
+    searchExact(input1) {
+        if (!input1 || typeof input1 !== 'string') {
+          return "Please enter a valid search input.";
+        }
+    // Convert the input1 to lowercase for case-insensitive matching 
+        input1 = input1.toLowerCase(); 
+        let resultsOfExactSearch = this.#data.filter(country => country.toLowerCase() === input1);
+    
+        if (resultsOfExactSearch.length === 0) {
+          return `No results found for "${input1}".`;
+        }
+    
+        return resultsOfExactSearch;
+      }
+    
 }
 
 let countryData = ["palestine", "jordan", "syria", "lebanon"];
@@ -77,7 +96,12 @@ console.log(countries.getSortedData());
 countries.decreasingSort(); // Sort the array in decreasing order
 console.log("Sorted array in decreasing order:");
 console.log(countries.getSortedData());
-let searchItem = "r"; // the output will be syria and jordan
+let searchItem = "i"; // the output will be syria and jordan
 let searchResults = countries.searchByCountry(searchItem);
 console.log(`Search results for "${searchItem}":`);
 console.log(searchResults);
+
+let searchItem2 = "iraq"; 
+let exactSearchResults = countries.searchExact(searchItem2);
+console.log(`Exact Search results : "${searchItem2}":`);
+console.log(exactSearchResults);
